8ae7743aa7d844301ba0a3c70cde35c7128e3417
Charles <charlesknapp00@gmail.com>
2024-09-20 19:05:01 -0400

    Removed unwanted code

--- src/app/(default)/articles/page.tsx
+++ src/app/(default)/articles/page.tsx
@@ -1,14 +1,12 @@
-import ArticlesCategoryList from "@/components/elements/ArticlesCategoryList"
-import ArticlesHero from "@/components/elements/ArticlesHero";
-import { getArticles } from "@/lib/articles"; // Function to fetch articles
+import { getArticles } from "@/lib/articles" // Function to fetch articles
+import ArticlesHero from "@/components/elements/ArticlesHero"
 
 export default function Articles() {
-  const articles = getArticles(); // Fetch article data, including cover images
+  const articles = getArticles() // Fetch article data, including cover images
 
   return (
     <>
       <ArticlesHero articles={articles} /> {/* Pass only articles */}
-      <ArticlesCategoryList />
     </>
   )
 }
