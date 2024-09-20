71b8f76eff53c8869e6f60861face761f5b2c2ac
Charles <charlesknapp00@gmail.com>
2024-09-20 18:30:57 -0400

    changes to nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@ import createMDX from "@next/mdx"
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   distDir: "build",
   //output: "export",
